b77eddf45d61b455770d92e42891550367f6fd22
deprasny <deprasny@gmail.com>
2018-04-26 13:48:54 +0700

    first commit

---
+++ pages/contact.vue
@@ -0,0 +1,12 @@
+<!-- pages/contact.vue -->
+
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">Contact Me</h1>
+      <p>
+        You can follow me on Twitter: <a href="https://twitter.com/depras" target="_blank">@depras</a>
+      </p>
+    </div>
+  </section>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Depras Nur Yadi</h1>
+      <h2 class="subtitle">a Software Developer based in Bandung, Indonesia.</h2>
+    </div>
+  </section>
+</template>
